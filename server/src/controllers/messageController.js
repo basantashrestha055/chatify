985789883666f781c88769059262d2219d447610
basantashrestha055 <basanta.shrestha055@gmail.com>
2025-09-15 17:15:04 +0545

    message routes added

---
+++ server/src/controllers/messageController.js
@@ -0,0 +1,108 @@
+import Message from '../models/Message.js';
+import User from '../models/User.js';
+import cloudinary from '../lib/cloudinary.js';
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select('-password');
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log('Error in getAllContacts controller', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const getMessageByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const { id: userToChatId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log('Error in getMessageByUserId controller', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const senderId = req.user._id;
+    const { id: receiverId } = req.params;
+
+    if (!text && !image) {
+      return res.status(400).json({ message: 'Message or image is required' });
+    }
+
+    if (senderId.equals(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: 'Cannot send a message to yourself' });
+    }
+
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: 'Receiver not found' });
+    }
+
+    let imageUrl;
+
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log('Error in sendMessage controller', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    });
+
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ];
+
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select('-password');
+
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.log('Error in getChatPartners controller', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessageByUserId,
+  sendMessage,
+} from '../controllers/messageController.js';
+import { protectRoute } from '../middleware/authMiddleware.js';
+import { arcjetProtection } from '../middleware/arcjetMiddleware.js';
 
 const router = express.Router();
 
-router.get('/sendmessage', (req, res) => {
-  res.send('sendmessage');
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get('/contacts', getAllContacts);
+router.get('/chats', getChatPartners);
+router.get('/:id', getMessageByUserId);
+router.post('/send/:id', sendMessage);
 
 export default router;
